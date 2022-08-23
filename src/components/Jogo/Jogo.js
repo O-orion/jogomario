a98219e4097bf731fecd7ddefb92b739ad04bc1b
Luca Viana <78035167+O-orion@users.noreply.github.com>
2022-08-23 09:23:14 -0400

    JESUS CRISTO É O SENHOR - Animando o jogo

--- src/components/Jogo/Jogo.js
+++ src/components/Jogo/Jogo.js
@@ -3,11 +3,21 @@ import './Jogo.css'
 
 // Importando imagens
 import imgNuvem from '../assets/clouds.png'
+import imgCano from '../assets/pipe.png'
+import imgMario from '../assets/mario.gif'
 
 function Jogo() {
+
+  document.onkeydown = function () {
+    console.log('On keyDown')
+  }
+
   return (
     <div className='jogo'>
-        <img className='nuvens' src={imgNuvem} alt="" />
+        <img className='nuvens' src={imgNuvem} alt="Imagem de nuvens" />
+        <img className='cano' src={imgCano} alt="Imagem de cano" />
+        <img className='mario' src={imgMario} alt="Imagem do mário" />
+        <div className='chao'></div>
     </div>
   )
 }
