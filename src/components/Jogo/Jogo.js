03e0097ba5ecfceb999a16c290b2636b86f7be8b
Luca Viana <78035167+O-orion@users.noreply.github.com>
2022-08-23 11:09:24 -0400

    JESUS CRISTO É O SENHOR - Implementando lógica de game over

--- src/components/Jogo/Jogo.js
+++ src/components/Jogo/Jogo.js
@@ -1,4 +1,7 @@
-import React from 'react'
+// Importando componentes do react e outras libs
+import React, { useState, useRef } from 'react'
+
+// Importando css
 import './Jogo.css'
 
 // Importando imagens
@@ -7,16 +10,59 @@ import imgCano from '../assets/pipe.png'
 import imgMario from '../assets/mario.gif'
 
 function Jogo() {
+  const [ estaPulando, setEstaPulando ] = useState(false)
+  let marioClassName = 'mario'
+  const marioRef = useRef();
+  const canoRef = useRef();
+
+
+
+  function marioEstaNoCano(){
+
+    const mario = marioRef.current;
+    const cano = canoRef.current;
+
+    if(!mario || !cano){
+      return;
+    }
+
+    return (
+      cano.offsetLeft > mario.offsetLeft &&
+      cano.offsetLeft < mario.offsetLeft + mario.offsetWidth && 
+      mario.offsetTop + mario.offsetHeight > cano.offsetTop
+    )
+
+  }
+
+  setInterval(function(){
+    const valor = marioEstaNoCano();
+    console.log('Mário está no cano ?', valor)
+  }, 100)
 
   document.onkeydown = function () {
-    console.log('On keyDown')
+    if(!estaPulando){
+
+      console.log('On keyDown: ' +  estaPulando)
+      setEstaPulando(true)
+
+      //retornando o estaPulando pra false
+      setTimeout(function(){
+        setEstaPulando(false)
+      }, 700)
+
+    }
+
+  }
+
+  if (estaPulando ){
+    marioClassName = "mario mario-pulo"
   }
 
   return (
     <div className='jogo'>
         <img className='nuvens' src={imgNuvem} alt="Imagem de nuvens" />
-        <img className='cano' src={imgCano} alt="Imagem de cano" />
-        <img className='mario' src={imgMario} alt="Imagem do mário" />
+        <img ref={canoRef} className='cano' src={imgCano} alt="Imagem de cano" />
+        <img ref={marioRef} className={ marioClassName } src={imgMario} alt="Imagem do mário" />
         <div className='chao'></div>
     </div>
   )
