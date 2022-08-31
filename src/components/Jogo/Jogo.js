351cd6bae004b4cf85df859bb76dc1687d4b16a0
Luca Viana <78035167+O-orion@users.noreply.github.com>
2022-08-31 09:41:31 -0400

    JESUS CRISTO É O SENHOR - Sistema de  pontos

--- src/components/Jogo/Jogo.js
+++ src/components/Jogo/Jogo.js
@@ -1,5 +1,5 @@
 // Importando componentes do react e outras libs
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 // Importando css
 import './Jogo.css'
@@ -8,14 +8,21 @@ import './Jogo.css'
 import imgNuvem from '../assets/clouds.png'
 import imgCano from '../assets/pipe.png'
 import imgMario from '../assets/mario.gif'
+import imgGameOver from '../assets/game-over.png'
 
 function Jogo() {
+  
+  // Estados
   const [ estaPulando, setEstaPulando ] = useState(false)
+  const [ estaMorto, setEstaMorto ] = useState(false);
+  const [ pontos, setPontos ] = useState(0);
+
+  // classe
   let marioClassName = 'mario'
+  // referências
+
   const marioRef = useRef();
   const canoRef = useRef();
-
-
 
   function marioEstaNoCano(){
 
@@ -27,17 +34,39 @@ function Jogo() {
     }
 
     return (
-      cano.offsetLeft > mario.offsetLeft &&
-      cano.offsetLeft < mario.offsetLeft + mario.offsetWidth && 
-      mario.offsetTop + mario.offsetHeight > cano.offsetTop
+      cano.offsetLeft > mario.offsetLeft +10 &&
+      cano.offsetLeft < mario.offsetLeft +10 + mario.offsetWidth  && 
+      mario.offsetTop +10 + mario.offsetHeight +10> cano.offsetTop
     )
 
   }
 
   setInterval(function(){
-    const valor = marioEstaNoCano();
-    console.log('Mário está no cano ?', valor)
+    const estaNoCano = marioEstaNoCano();
+    //console.log('Mário está no cano ?', valor)
+
+    if(!estaNoCano){
+      return;
+    }
+    setEstaMorto(true);
+
+
   }, 100)
+
+  // UseEfect evitando que o setInterval seja recroadp várias vezes
+  useEffect(function(){
+
+      const interval = setInterval(() => {
+        if( estaMorto ){
+          return;
+        }
+    
+        setPontos(pontos + 1)
+        console.log( { pontos } )
+      }, 500)
+
+      return () => clearInterval(interval)
+  }, [estaMorto, pontos])
 
   document.onkeydown = function () {
     if(!estaPulando){
@@ -58,11 +87,14 @@ function Jogo() {
     marioClassName = "mario mario-pulo"
   }
 
+  const marioImagem = estaMorto ? imgGameOver: imgMario
+  const pararAnimacao = estaMorto ?  "parar-animacao": "";
+
   return (
     <div className='jogo'>
-        <img className='nuvens' src={imgNuvem} alt="Imagem de nuvens" />
-        <img ref={canoRef} className='cano' src={imgCano} alt="Imagem de cano" />
-        <img ref={marioRef} className={ marioClassName } src={imgMario} alt="Imagem do mário" />
+        <img className={"nuvens " + pararAnimacao} src={imgNuvem} alt="Imagem de nuvens" />
+        <img ref={canoRef} className={ "cano " + pararAnimacao } src={imgCano} alt="Imagem de cano" />
+        <img ref={marioRef} className={ marioClassName } src={marioImagem} alt="Imagem do mário" />
         <div className='chao'></div>
     </div>
   )
